8cf8c0f14e1342596d02884d532d0fb94f2b92ce
SamEngComp <salessamuel268@gmail.com>
2021-07-23 11:34:53 -0300

    remover files

--- ModalCustomizationFramework/ModalCustomizationFramework/Controller/NavigationViewController.m
+++
@@ -1,8 +0,0 @@
-//
-//  NavigationViewController.m
-//  ModalCustomizationFramework
-//
-//  Created by Jhennyfer Rodrigues de Oliveira on 21/07/21.
-//
-
-#import <Foundation/Foundation.h>
--- ModalCustomizationFramework/ModalCustomizationFramework/View/BlurView.m
+++
@@ -1,8 +0,0 @@
-//
-//  BlurView.m
-//  ModalCustomizationFramework
-//
-//  Created by Jhennyfer Rodrigues de Oliveira on 21/07/21.
-//
-
-#import <Foundation/Foundation.h>
