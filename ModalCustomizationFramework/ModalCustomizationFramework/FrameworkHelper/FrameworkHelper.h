c105935f30745564ecf35d1b4282e6a17a81a509
Jhennyfer Rodrigues de Oliveira <jhennyfer519@gmail.com>
2021-07-26 11:16:56 -0300

    adding new file

---
+++ ModalCustomizationFramework/ModalCustomizationFramework/FrameworkHelper/FrameworkHelper.h
@@ -0,0 +1,12 @@
+//
+//  FrameworkHelper.h
+//  ModalCustomizationFramework
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 26/07/21.
+//
+
+#ifndef FrameworkHelper_h
+#define FrameworkHelper_h
+
+
+#endif /* FrameworkHelper_h */
---
+++ ModalCustomizationFramework/ModalCustomizationFramework/FrameworkHelper/FrameworkHelper.m
@@ -0,0 +1,8 @@
+//
+//  FrameworkHelper.m
+//  ModalCustomizationFramework
+//
+//  Created by Jhennyfer Rodrigues de Oliveira on 26/07/21.
+//
+
+#import <Foundation/Foundation.h>
