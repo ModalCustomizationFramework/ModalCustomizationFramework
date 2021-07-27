28def230d6fcfd76610167209bc299b1b1e9a378
SamEngComp <salessamuel268@gmail.com>
2021-07-27 10:01:19 -0300

    modal scale state

---
+++ ModalCustomizationFramework/ModalCustomizationFramework/Controller/ModalTransitioningDelegate.m
@@ -0,0 +1,37 @@
+//
+//  ModalTransitionDelegate.m
+//  AppObjc
+//
+//  Created by Samuel Sales on 23/07/21.
+//
+
+#import "ModalTransitioningDelegate.h"
+
+@implementation ModalTransitioningDelegate
+
+-(instancetype)initWithViewController:(UIViewController*)viewController
+             presentingViewController:(UIViewController*)presentingViewController
+                      modalScaleState:(ModalScaleState)modalScaleState
+                          isExpansive:(BOOL)isExpansive
+{
+    if(self = [super init])
+    {
+        self.viewController = viewController;
+        self.presentingViewController = presentingViewController;
+        self.state = modalScaleState;
+        self.isExpansive = isExpansive;
+    }
+    return self;
+}
+
+-(UIPresentationController*)presentationControllerForPresentedViewController:(UIViewController*)presented
+                                                    presentingViewController:(UIViewController*)presenting sourceViewController:(UIViewController *)source
+{
+    return [[ModalPresenter alloc] initWithPresentedViewController:presented
+                                          presentingViewController:presenting
+                                                   modalScaleState:self.state
+                                                       isExpansive:self.isExpansive];
+}
+
+@end
+
--- ModalCustomizationFramework/ModalCustomizationFramework/ModalCustomizationFramework.h
+++ ModalCustomizationFramework/ModalCustomizationFramework/ModalCustomizationFramework.h
@@ -15,7 +15,6 @@ FOUNDATION_EXPORT const unsigned char ModalCustomizationFrameworkVersionString[]
 
 #import <ModalCustomizationFramework/ModalPresenter.h>
 #import <ModalCustomizationFramework/ModalTransitioningDelegate.h>
-#import <ModalCustomizationFramework/ModalViewController.h>
 // In this header, you should import all the public headers of your framework using statements like #import <ModalCustomizationFramework/PublicHeader.h>
 
 
